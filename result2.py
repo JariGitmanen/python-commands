8409384851f254620e4a158feb10bd42a174d5ed
JariGitmanen <50591289+JariGitmanen@users.noreply.github.com>
2019-05-24 09:57:45 +0100

    Add files via upload

---
+++ result2.py
@@ -0,0 +1,12 @@
+physics=80
+chemistry=100
+math=95
+total=physics+chemistry+math
+per=total*100/450
+print("physics: ",physics)
+print("chemistry: ",chemistry)
+print("math: ",math)
+print("-----------------------")
+print("total marks: ",total)
+print("percentage: ",per)
+print("-----------------------")
