2b1f48197c7e83eddc6cf857bcadd5c645d61cba
JariGitmanen <50591289+JariGitmanen@users.noreply.github.com>
2019-05-24 11:25:43 +0100

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,7 @@
+physics=input("enter physics marks:")
+chemistry=input("enter chemistry marks:")
+maths=input("enter maths marks:")
+total=int(physics)+int(chemistry)+int(maths)
+percentage=int(total)*100/150
+print("The total marks are:",total)
+print("The total percentage is:",percentage)
